4ca494a006a562726a9348e519297c5d70a29b65
DESKTOP-CAQ9A79\JYUN3 <jacob.yun429@gmail.com>
2018-05-05 12:42:57 -0700

    added in game logic and victory, need to add reset button

--- assets/tictactoe.js
+++ assets/tictactoe.js
@@ -1,37 +1,46 @@
-class TicTacToe {
-    constructor(selector) {
-        this.rows = 3;
-        this.cols = 3;
-        this.selector = selector;
-        this.move = 1;
-        this.play = true;
-        this.createBoard();
-    }
+$(document).ready(function() {
 
-    createBoard() {
-        const $board = $(this.selector);
-        for (let row = 0; row <this.rows; row++) {
-            const $row = $('<div>')
-              .addClass('row');
-            for (let col = 0; col < this.cols; col++) {
-                const $col = $('<div>')
-                  .addClass('col empty');
-                  $row.append($col);
-            }
-            $board.append($row);
+    var move = 1;
+    var play = true;
+
+    $("#board tr td").click(function() {
+        if ($(this).text()=="" && play) {
+          if ((move%2)==1) { $(this).append("X"); } 
+          else { $(this).append("O"); }
+          move++;
+          if (winner()!=-1 && winner()!="") {
+              if (winner()=="X") { $("#messages").append("Player 1 wins!"); }
+              else { $("#messages").append("Player 2 wins!"); }
+              play = false;
+          }
         }
+      });
+
+    function winner() {
+        var space1 = $("#board tr:nth-child(1) td:nth-child(1)").text();
+        var space2 = $("#board tr:nth-child(1) td:nth-child(2)").text();
+        var space3 = $("#board tr:nth-child(1) td:nth-child(3)").text();
+        var space4 = $("#board tr:nth-child(2) td:nth-child(1)").text();
+        var space5 = $("#board tr:nth-child(2) td:nth-child(2)").text();
+        var space6 = $("#board tr:nth-child(2) td:nth-child(3)").text();
+        var space7 = $("#board tr:nth-child(3) td:nth-child(1)").text();
+        var space8 = $("#board tr:nth-child(3) td:nth-child(2)").text();
+        var space9 = $("#board tr:nth-child(3) td:nth-child(3)").text();
+        //check rows
+        if ((space1==space2) && (space2==space3)) { return space3; }
+        else if ((space4==space5) && (space5==space6)) { return space6; }
+        else if ((space7==space8) && (space8==space9)) { return space9; }
+        // check columns
+        else if ((space1==space4) && (space4==space7)) { return space7; }
+        else if ((space2==space5) && (space5==space8)) { return space8; }
+        else if ((space3==space6) && (space6==space9)) { return space9; }
+        // check diagonals
+        else if ((space1==space5) && (space5==space9)) { return space9; }
+        else if ((space3==space5) && (space5==space7)) { return space7; }
+        // no winner
+        return -1;   
     }
 
-    setupEventListeners() {
-        const $board = $(this.selector);
-        const that = this;
+    
 
-        $board.on('click', '.col.empty', function() {
-            if (that.text()=="" && play) {
-                if ((move%2)==1) { that.append("X"); }
-                else { $that.append("O"); }
-            move++;
-            }
-        });
-    }
-};
+});
